be85f82de16adb8e7032b3c5879c9a6d0f1c6095
isabelrubioarroyo-lgtm <isabelrubioarroyo@gmail.com>
2026-01-06 13:48:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = 'PEGA_AQUI_TU_TOKEN';
+mapboxgl.accessToken = 'pk.eyJ1IjoiaXNhYmVscnViaW9hIiwiYSI6ImNtanNzZ2h2OTJmMWszZHNkYWN6dzczMmUifQ.b5XfE8S-qEIoFG6FNPifvQ';
 
 const map = new mapboxgl.Map({
   container: 'map',
